8594480e28461ce1e92f20e93bb27ea6687aaf12
maks0nch1k <maksimf647@gmail.com>
2023-04-11 19:42:38 +0300

    register form

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from data import db_session
 from data.users import User
-from data.jobs import Jobs
+from forms.user import RegisterForm
+from flask import render_template, redirect
 
 
 app = Flask(__name__)
@@ -9,7 +10,36 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    app.run()
+    db_session.global_init("db/blogs.db")
+    app.run(port=5000, host='127.0.0.1')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form, message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form, message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            hashed_password=form.password.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
