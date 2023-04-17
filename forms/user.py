c2312ad81ffdb2f3b302625b87fcda2b45a435b0
maks0nch1k <maksimf647@gmail.com>
2023-04-17 17:43:16 +0300

    login

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, EmailField, IntegerField
+from wtforms import PasswordField, StringField, SubmitField, EmailField, IntegerField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -14,3 +14,10 @@ class RegisterForm(FlaskForm):
     speciality = StringField("Speciality", validators=[DataRequired()])
     address = StringField("Address", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -2,17 +2,41 @@ from flask import Flask
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from flask import render_template, redirect
+from flask_login import LoginManager, login_user
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
     db_session.global_init("db/blogs.db")
     app.run(port=5000, host='127.0.0.1')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route("/")
